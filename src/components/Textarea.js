2ba9ec5bca6521defec4b8e377944c6366939076
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-04 18:07:07 -0400

    finished textinput and textarea components

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,28 +1,28 @@
 import propTypes from 'prop-types';
 import styled from 'styled-components';
 
-const TextInput = ({ onChange, value, name, label }) => (
+const Textarea = ({ onChange, value, name, label }) => (
   <StyledLabel htmlFor={name}>
     {label}
-    <StyledTextInput
+    <StyledTextarea
       value={value}
       type="text"
       name={name}
       onChange={onChange}
-      data-testid={`${name}TextInput`}
+      data-testid={`${name}Textarea`}
     />
   </StyledLabel>
 );
 
 const StyledLabel = styled.label``;
 
-const StyledTextInput = styled.input``;
+const StyledTextarea = styled.textarea``;
 
-TextInput.propTypes = {
+Textarea.propTypes = {
   onChange: propTypes.func.isRequired,
   value: propTypes.bool.isRequired,
   name: propTypes.string.isRequired,
   label: propTypes.string.isRequired,
 };
 
-export default TextInput;
+export default Textarea;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import TextInput from './TextInput';
 import Button from './Button';
 import Checkbox from './Checkbox';
+import Textarea from './Textarea';
 
-export { TextInput, Button, Checkbox };
+export { TextInput, Button, Checkbox, Textarea };
