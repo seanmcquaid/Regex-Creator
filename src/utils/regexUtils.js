c2b33c10f29257cd9b63cc59b037a0a99284d267
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-04 19:28:30 -0400

    starting to work on regex utils

--- src/utils/regexUtils.js
+++ src/utils/regexUtils.js
@@ -1,3 +1,29 @@
-export const createRegex = (params) => {
+export const createRegex = ({
+  characters = '',
+  includeCharacters = false,
+  isGlobal = false,
+  isCaseInsensitive = false,
+  isStart = false,
+  isEnd = false,
+  onlyNumbers = false,
+  onlyLetters = false,
+}) => {
+  if (onlyNumbers) {
+    return '[0-9]';
+  }
+  if (onlyLetters) {
+    return '[a-zA-Z]+';
+  }
+  if (isStart) {
+    return '^';
+  }
+  if (isEnd) {
+    return '$';
+  }
   return '';
 };
+
+export const addPatternModifier = (
+  isGlobal = false,
+  isCaseInsensitive = false,
+) => {};
